79646405793a7032119ef34a1ff01deae1eacaa8
susurro0 <us.cingoez@gmail.com>
2021-09-10 15:52:10 -0400

    create function makeCacheMatrix: create special matrix, which contains list of functions (get/ set matrix, get/set inverse)
    create function cacheSolve: calculate inverse, by checking cache first. if exists result out of cache. Else calculate inverse and save it in cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## create special "matrix", which contains a list of functions to:
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                inv_matrix <<- NULL
+        }
+        get <- function() x
+        set_inverse <- function(inverse) inv_matrix <<- inverse
+        get_inverse <- function() inv_matrix
+        list(set = set, 
+             get = get,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## calculate the inverse of matrix, by checking if inverse of matrix 
+## already exists in cache. If so, return value from cache. Otherwise 
+## calculate inverse and save it in cache with the function set_inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_matrix <- x$get_inverse()
+        ##check if inverse in cache
+        if(!is.null(inv_matrix)) {
+                message("getting cached data")
+                return(inv_matrix)
+        }
+        ##get matrix
+        data <- x$get()
+        ## compute the inverse of a matrix alternative: ginv() but solve is faster
+        inv_matrix <- solve(data, ...)
+        #save inverse in cache
+        x$set_inverse(inv_matrix)
+        #return inverse matrix
+        inv_matrix
 }
